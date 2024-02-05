e9d631bddc3ca9303a934b2a4edaaa5b57a34f03
user <popovamariia2809@gmail.com>
2024-02-05 23:43:50 +0700

    Initial commit

---
+++ Monolit-master/monolit/shop/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from .models import *
+
+class RegistrationForm(UserCreationForm):
+    model = CustomUser
+    password1 = forms.CharField(label='Введите пароль', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput())
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'password1', 'password2', 'avatar']
+
+        labels = {
+            'username': 'Логин',
+            'avatar': 'Ава',
+            'email': 'Почта'
+        }
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'avatar']
